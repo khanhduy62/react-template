ae19fd7cd351621f2d3a5efd6b8810e36e2be7a7
Le Khanh Duy <lekhanhduyduy1201@gmail.com>
2021-10-03 16:52:33 +0700

    asset resource

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -22,7 +22,11 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+         test: /\.(png|svg|jpg|jpeg|gif)$/i,
+         type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
     ],
