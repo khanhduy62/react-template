24ad5b36f206fa0fefba782176ba8d07b638b844
Le Khanh Duy <lekhanhduyduy1201@gmail.com>
2021-10-03 16:46:31 +0700

    css loader

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import './styles.css';
+// import AVATAR from './duy-avatar.png';
+
 export const App = () => {
-  return <h1>React App Template</h1>
+  return <>
+    <h1>React App Template</h1>
+    {/* <img src={AVATAR} alt="My Avatar" /> */}
+  </>
 }
