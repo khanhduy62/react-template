4068bb2372b25f040b87089153b6588e905b5382
Duy Le <duyle@hmsfirst.com>
2021-10-12 17:07:17 +0700

    setup from scratch 7/8: husky

--- src/Counter.js
+++ src/Counter.js
@@ -2,8 +2,6 @@ import { useState } from 'react';
 
 const Counter = () => {
   const [count, setCount] = useState(0);
-  const name = 'DUY';
-
   return (
     <div>
       <button onClick={() => setCount((c) => c + 1)}>Count {count}</button>
